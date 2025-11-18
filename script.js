6959a781892a5b2a59a7a68197624239701f38a9
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-18 17:04:31 +0100

    feat: esercizio completato

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
- fetch("https://lanciweb.github.io/demo/api/pictures/")
- .then(Response => Response.json())
- .then (data =>{
-     const container = document.querySelector("conjtainer-2");
-     data.forEach(obj => {
-        
-         const img = document.createElement("img")
-         img.src= obj.url;
-        
-         const description= document.createElement("p");
-                  description.innerText= obj.date
-        
-  const title= document.createElement("h2")
-         title.innerText=obj.title
-        
-         card.appendChild(img);
-        card.appendChild(description);
-         card.appendChild(title);
-         container.appendChild(card)
-     });
- })
+const grid = document.getElementById("post-grid")
+
+
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
+.then((resp)=>{
+    const array = resp.data
+    let postStr="";
+    array.forEach((curPost)=>{
+    postStr += `<div class="card">
+                 <img src="./img/pin.svg" alt="" class="pin"> 
+                <img src="${curPost.url}" alt="" class="immagine">
+                <p>${curPost.date}</p>
+                <h2>${curPost.title}</h2>
+            </div>`
+    })
+     grid.innerHTML = postStr
+})
+
 
