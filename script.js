1707ba65144bb78ff2c6d900f08956fe51b14d59
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-18 13:07:23 +0100

    feat: ancora da completare

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-fetch("https://lanciweb.github.io/demo/api/pictures/")
-.then(Response => Response.json())
-.then (data =>{
-    const container = document.querySelector("conjtainer-2");
-    data.forEach(obj => {
-        const card = document.querySelector("card")
-        const img = document.createElement("img")
-        const description= document.createElement("p")
-        const title =  document.
-        img.src= obj.url;
-    });
-})
+ fetch("https://lanciweb.github.io/demo/api/pictures/")
+ .then(Response => Response.json())
+ .then (data =>{
+     const container = document.querySelector("conjtainer-2");
+     data.forEach(obj => {
+        
+         const img = document.createElement("img")
+         img.src= obj.url;
+        
+         const description= document.createElement("p");
+                  description.innerText= obj.date
+        
+  const title= document.createElement("h2")
+         title.innerText=obj.title
+        
+         card.appendChild(img);
+        card.appendChild(description);
+         card.appendChild(title);
+         container.appendChild(card)
+     });
+ })
+
